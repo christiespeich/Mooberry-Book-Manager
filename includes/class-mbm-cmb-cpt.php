c9c06b438e19d8ba1bd4d4e5f9f020d1aa6645a6
Christie Speich <christiespeich@gmail.com>
2017-10-15 09:02:20 -0400

    abstract the get_cpt for cmb cpts

--- includes/class-mbm-cmb-cpt.php
+++ includes/class-mbm-cmb-cpt.php
@@ -1,15 +1,38 @@
 <?php
-class MBDB_CMB_CPT implements iMooberry_Book_Manager_Data_Storage_Behavior {
+abstract class MBDB_CMB_CPT implements iMooberry_Book_Manager_Data_Storage_Behavior {
 	protected $post_type;
+	
+	abstract protected function  add_post_meta( $object, $postmeta );
 	
 	public function __construct() {
 		add_action( 'before_delete_post', array( $this, 'post_deleted') );
 	}
 	
+	// gets all data for a custom post type
+	// only this class knows how everything is stored in the datbase
+	// so when another class needs an object of this CPT, we need to get all the info
+	// and return it as an object
+	// id can be an id from the posts table
+	// or it can be an array of postmeta fields if coming from the preview button
 	public function get( $id, $cache_results = true ) {
-		// TODO Add caching
-		$data = get_post( $id );
-		return $data;
+	
+		if ( !is_array( $id ) ) {
+			//$object = parent::get( $id, $cache_results );
+			$object = get_post( $id );
+		
+			if ( $object == null ) {
+				return null;
+			}
+			// TODO add cachign?
+			$postmeta = get_post_meta( $id );
+		} else {
+			$postmeta = $id;
+			
+			$object = parent::get( $postmeta['id'], $cache_results ); //get_post ( $postmeta['id'] );
+			
+		}
+		$object = $this->add_post_meta( $object, $postmeta );
+		return $object;
 	}
 	
 	public function get_data( $data_field, $id, $cache_results = true ) {
