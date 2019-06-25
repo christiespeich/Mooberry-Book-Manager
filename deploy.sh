c152ebed91baac49aa03de000bf5f60d6dab06f5
Christie Speich <christiespeich@gmail.com>
2019-06-24 23:06:56 -0400

    changed name of ibooks to apple books

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
  
 # main config
-PLUGINSLUG=“Mooberry-book-manager”
+PLUGINSLUG="mooberry-book-manager"
 CURRENTDIR=`pwd`
-MAINFILE=“mooberry-book-manager.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="mooberry-book-manager.php" # this should be the name of your main php file in the wordpress plugin
  
 # git config
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
@@ -13,7 +13,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER=“mooberrydreams” # your svn username
+SVNUSER="mooberrydreams" # your svn username
  
  
 # Let's begin...
