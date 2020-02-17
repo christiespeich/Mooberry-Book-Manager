eb06ae3a54e2f9640bf37e42259134a1fe812f88
Christie Speich <christiespeich@gmail.com>
2020-02-16 20:45:56 -0500

    fix bug on no books found filter

--- mooberry-book-manager.php
+++ mooberry-book-manager.php
@@ -6,7 +6,7 @@
   *  Author: Mooberry Dreams
   *  Author URI: http://www.mooberrydreams.com/
   *  Donate Link: https://www.paypal.me/mooberrydreams/
-  *  Version: 4.2
+  *  Version: 4.2.1
   *  Text Domain: mooberry-book-manager
   *  Domain Path: languages
   *
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  //error_log('starting');
 // Plugin version
 if ( ! defined( 'MBDB_PLUGIN_VERSION' ) ) {
-	define( 'MBDB_PLUGIN_VERSION', '4.2' );
+	define( 'MBDB_PLUGIN_VERSION', '4.2.1' );
 }
 
 if ( ! defined( 'MBDB_PLUGIN_VERSION_KEY' ) ) {
