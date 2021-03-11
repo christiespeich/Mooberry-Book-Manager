d2aa5daec6ba65318c26c855849da6893c93ae41
Christie Speich <christiespeich@gmail.com>
2021-03-11 12:58:43 -0500

    wordpress 5.7

--- includes/admin/js/admin-options.js
+++ includes/admin/js/admin-options.js
@@ -1,31 +1,31 @@
 jQuery( document ).ready(function() {
-	
-	jQuery('#reset_meta_boxes').click(function (e) {
+
+	jQuery('#reset_meta_boxes').on ('click', function (e) {
 			if (confirm(mbdb_admin_options_ajax.translation)) {
 				reset_meta_boxes();
 			}
 	});
-	
-	jQuery('#mbdb_cancel_import').click(function (e) {
+
+	jQuery('#mbdb_cancel_import').on( 'click', function (e) {
 		if ( confirm(mbdb_admin_options_ajax.cancel_import_translation ) ) {
 			mbdb_cancel_import();
 		}
 	});
-	
+
 	jQuery('#mbdb_create_tax_grid_page_button').on( 'click', mbdb_create_tax_grid_page );
-	
-		
-		
-		
+
+
+
+
 	/*
 	jQuery('#mbdb_settings_metabox').submit(function (e) {
 		var bad_name = [];
-		
+
 		// data_obj comes from PHP
 		var reserved_terms = data_obj.reserved_terms;
 		var flag = false;
 		jQuery('input[type=text][id$=slug]').each(function (idx) {
-			if (jQuery.inArray( this.value, reserved_terms ) > -1 ) {			
+			if (jQuery.inArray( this.value, reserved_terms ) > -1 ) {
 				jQuery(this).css('border', 'solid 1px red');
 				bad_name.push(jQuery(this).parent().prev().children('label[for=' + this.id + ']').text()); //.text());
 			}
@@ -38,30 +38,30 @@ jQuery( document ).ready(function() {
 });
 /*
 function popup( link ) {
-	if (! window.focus) return true; 
-	var href; 
-	if (typeof(mylink) == 'string') 
-		href=mylink; 
-	else 
-		href=mylink.href; 
-	window.open(href, 'Reserved Terms', 'width=400,height=200,scrollbars=yes'); 
+	if (! window.focus) return true;
+	var href;
+	if (typeof(mylink) == 'string')
+		href=mylink;
+	else
+		href=mylink.href;
+	window.open(href, 'Reserved Terms', 'width=400,height=200,scrollbars=yes');
 	return false;
 }
 */
 function reset_meta_boxes() {
 
-	
+
 	var data = {
 			'action': 'mbdb_reset_meta_boxes',
 			'security': mbdb_admin_options_ajax.ajax_nonce
 	};
 	var ajax = jQuery.post(mbdb_admin_options_ajax.ajax_url, data);
-	
+
 	ajax.done( function ( data ) {
 		jQuery('#reset_complete').show();
 		 jQuery('#reset_progress').hide();
 	});
-	
+
 }
 
 function mbdb_cancel_import() {
@@ -75,7 +75,7 @@ function mbdb_cancel_import() {
 		jQuery('#mbdb_cancel_import_progress').hide();
 		jQuery('#mbdb_cancel_results').empty().append( data);
 	});
-	
+
 }
 
 function mbdb_create_tax_grid_page() {
@@ -85,7 +85,7 @@ function mbdb_create_tax_grid_page() {
 	};
 	jQuery('#mbdb_create_tax_grid_page_progress').show();
 	var ajax = jQuery.post( mbdb_admin_options_ajax.ajax_url, data);
-	ajax.done ( function ( data ) { 
+	ajax.done ( function ( data ) {
 		jQuery('#mbdb_create_tax_grid_page_progress').hide();
 		if ( data == 0 ) {
 			result = mbdb_admin_options_ajax.create_tax_grid_page_fail_translation;
