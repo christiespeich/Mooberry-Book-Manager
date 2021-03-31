fab6f47992491c25ee1f6ec42dc41ec12fd48a07
Christie Speich <christiespeich@gmail.com>
2021-03-31 13:48:54 -0400

    add back to items label

--- includes/class-mbm-taxonomy.php
+++ includes/class-mbm-taxonomy.php
@@ -11,42 +11,42 @@
  * The Mooberry Book Manager Taxonomy class is the base class responsible for creating and managing
  * taxonomies
  *
- * 
+ *
  *
  * @since    4.0.0
  */
 class Mooberry_Book_Manager_Taxonomy {
-	
+
 	protected $slug;
-	
+
 	protected $post_type;
-	
+
 	protected $singular_name;
-	
+
 	protected $plural_name;
-	
+
 	protected $args;
-	
+
 	public function __construct( $slug, $post_type, $singular_name = '', $plural_name = '', $args = array() ) {
-		
+
 		$this->slug = $slug;
-		
+
 		$this->post_type = $post_type;
-		
+
 		$this->singular_name = ( $singular_name != '' ? $singular_name : $slug );
-		
+
 		$this->plural_name = ( $plural_name != ''? $plural_name :  $this->singular_name );
-		
+
 		$defaults = array(
 				'rewrite' => array(	'slug' => 'mbdb_' . $this->plural_name ),
-				'public' => true, 
+				'public' => true,
 				'show_admin_column' => true,
 				'show_in_quick_edit'	=> 	true,
 				'update_count_callback' => '_update_post_term_count',
 				'capabilities'	=> array(
 					'manage_terms' => 'manage_categories',
 					'edit_terms'   => 'manage_categories',
-					'delete_terms' => 'manage_categories',	
+					'delete_terms' => 'manage_categories',
 				),
 				'labels' => array(
 					'name' => sprintf( __( '%s', 'mooberry-book-manager' ), $this->plural_name ),
@@ -67,20 +67,21 @@ class Mooberry_Book_Manager_Taxonomy {
 					'not_found' => sprintf( __( 'No %s found', 'mooberry-book-manager' ), strtolower( $this->plural_name ) ),
 					'items_list_navigation' => sprintf( __( '%s navigation', 'mooberry-book-manager' ), $this->singular_name ),
 					'items_list'            => sprintf( __( '%s list', 'mooberry-book-manager' ), $this->singular_name ),
+					'back_to_items' => sprintf( __( 'Back to %s', 'mooberry-book-manager' ), strtolower( $this->plural_name ) ),
 
 				)
 			);
-			
+
 		$this->args = wp_parse_args( $args, $defaults );
-		
+
 	}
-	
+
 	public function register() {
-		
+
 		register_taxonomy( $this->slug, $this->post_type, apply_filters( $this->slug . '_taxonomy', $this->args )	);
 	}
-	
-	
+
+
 	/**
 	 * Magic __get function to dispatch a call to retrieve a private property
 	 *
@@ -93,23 +94,23 @@ class Mooberry_Book_Manager_Taxonomy {
 			return call_user_func( array( $this, 'get_' . $key ) );
 
 		} else {
-			
+
 			if ( property_exists( $this, $key ) ) {
-				
+
 				$ungettable_properties = array( 'db_object' );
-				
+
 				if ( !in_array( $key, $ungettable_properties ) ) {
-				
+
 					return $this->$key;
 
 				}
-		
+
 			}
-		
+
 		}
-	
+
 		return new WP_Error( 'mbdb-invalid-property', sprintf( __( 'Can\'t get property %s', 'mooberry-book-manager' ), $key ) );
-				
+
 	}
-	
+
 }
