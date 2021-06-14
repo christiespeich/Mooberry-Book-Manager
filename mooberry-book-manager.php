6f8d4e2cbfbb294d35aecaa518759ff64660b64e
Christie Speich <christiespeich@gmail.com>
2021-06-14 11:21:37 -0400

    version update

--- mooberry-book-manager.php
+++ mooberry-book-manager.php
@@ -6,7 +6,7 @@
  *  Author: Mooberry Dreams
  *  Author URI: http://www.mooberrydreams.com/
  *  Donate Link: https://www.paypal.me/mooberrydreams/
- *  Version: 4.8.2
+ *  Version: 4.9
  *  Text Domain: mooberry-book-manager
  *  Domain Path: languages
  *
@@ -27,7 +27,7 @@
  *
  * @package MBDB
  * @author  Mooberry Dreams
- * @version 4.8.2
+ * @version 4.9
  */
 
 // Exit if accessed directly
@@ -38,7 +38,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 //error_log('starting');
 // Plugin version
 if ( ! defined( 'MBDB_PLUGIN_VERSION' ) ) {
-	define( 'MBDB_PLUGIN_VERSION', '4.8.2' );
+	define( 'MBDB_PLUGIN_VERSION', '4.9' );
 }
 
 if ( ! defined( 'MBDB_PLUGIN_VERSION_KEY' ) ) {
