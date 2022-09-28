066338b719813b0c8fb3dfbb91511a10a08d7288
Christie Speich <christiespeich@gmail.com>
2022-09-28 15:30:36 -0400

    export with UTF-8

--- includes/admin/class-mbm-core-settings.php
+++ includes/admin/class-mbm-core-settings.php
@@ -1687,7 +1687,8 @@ echo '<a id="mbdb_cancel_import" class="button" >' . __( 'Cancel Import', 'moobe
 		$file = apply_filters('mbdb_export_data_all_books_csv', $file, $books );
 
 		$fp = fopen(MBDB_PLUGIN_DIR . '/includes/admin/export.csv', 'w');
-
+   		//add BOM to fix UTF-8 in Excel
+    	fputs($fp, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
 		foreach ($file as $fields) {
 			fputcsv($fp, $fields);
 		}
