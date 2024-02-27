c084c2efb018737796ef62ef3ac3a51feb61754e
Christie Speich <christiespeich@gmail.com>
2024-02-27 12:23:29 -0500

    delete temp file

--- includes/export.php
+++ includes/export.php
@@ -2,3 +2,5 @@
 header('Content-disposition: attachment; filename=MBMexport.txt'); // . "\n";
 header( 'Content-type: text/plain' ); //. "\n\n";
 readfile('admin/export.txt');
+
+unlink('admin/export.txt');
--- includes/export_csv.php
+++ includes/export_csv.php
@@ -2,3 +2,5 @@
 header('Content-disposition: attachment; filename=MBMexport.csv'); // . "\n";
 header( 'Content-type: text/plain' ); //. "\n\n";
 readfile('admin/export.csv');
+
+unlink('admin/export.csv');
